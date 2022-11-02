ef1e9e39462e8189b5eb87e107c51521d82bae3c
paxsethorld <paxsethorld@gmail.com>
2022-11-01 20:27:48 -0500

    clean

--- src/networks/behavior/protocol.rs
+++ src/networks/behavior/protocol.rs
@@ -87,58 +87,58 @@ pub async fn process_swarm_events(
                 //     println!("Ping {:?} {:?}", peer, result);
                 // },
 
-                // // Autonat
-                // SwarmEvent::Behaviour(my_behavior::Event::Autonat(autonat::Event::InboundProbe(ip_event))) => {
-                //     println!("autonat inbound {:?}", ip_event);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::Autonat(autonat::Event::OutboundProbe(op_event))) => {
-                //     println!("autonat outbound {:?}", op_event);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::Autonat(autonat::Event::StatusChanged {old, new})) => {
-                //     println!("autonat status changed- old: {:?}, new: {:?}", old, new);
-                //     // if new status is private, act as client relay
-                //     // if new status is public, no need for holepunching and can start advertising listen address
-                //     // can also act as relay node
-                // },
+                // Autonat
+                SwarmEvent::Behaviour(my_behavior::Event::Autonat(autonat::Event::InboundProbe(ip_event))) => {
+                    println!("autonat inbound {:?}", ip_event);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::Autonat(autonat::Event::OutboundProbe(op_event))) => {
+                    println!("autonat outbound {:?}", op_event);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::Autonat(autonat::Event::StatusChanged {old, new})) => {
+                    println!("autonat status changed- old: {:?}, new: {:?}", old, new);
+                    // if new status is private, act as client relay
+                    // if new status is public, no need for holepunching and can start advertising listen address
+                    // can also act as relay node
+                },
 
-                // // Relay Client
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::ReservationReqAccepted {relay_peer_id, renewal, limit})) => {
-                //     println!("1 ReservationReqAccepted {:?}", relay_peer_id);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::ReservationReqFailed {relay_peer_id, renewal, error})) => {
-                //     println!("2 ReservationReqFailed {:?}", relay_peer_id);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::OutboundCircuitEstablished {relay_peer_id, limit})) => {
-                //     println!("3 OutboundCircuitEstablished {:?}", relay_peer_id);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::OutboundCircuitReqFailed {relay_peer_id, error})) => {
-                //     println!("4 OutboundCircuitReqFailed {:?}", relay_peer_id);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitEstablished {src_peer_id, limit})) => {
-                //     println!("5 InboundCircuitEstablished {:?}", src_peer_id);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitReqFailed {relay_peer_id, error})) => {
-                //     println!("6 InboundCircuitReqFailed {:?}", relay_peer_id);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitReqDenied {src_peer_id})) => {
-                //     println!("7 InboundCircuitReqDenied {:?}", src_peer_id);
-                // },
-                // SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitReqDenyFailed {src_peer_id, error})) => {
-                //     println!("8 InboundCircuitReqDenyFailed {:?}", src_peer_id);
-                // },
+                // Relay Client
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::ReservationReqAccepted {relay_peer_id, renewal, limit})) => {
+                    println!("1 ReservationReqAccepted {:?}", relay_peer_id);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::ReservationReqFailed {relay_peer_id, renewal, error})) => {
+                    println!("2 ReservationReqFailed {:?}", relay_peer_id);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::OutboundCircuitEstablished {relay_peer_id, limit})) => {
+                    println!("3 OutboundCircuitEstablished {:?}", relay_peer_id);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::OutboundCircuitReqFailed {relay_peer_id, error})) => {
+                    println!("4 OutboundCircuitReqFailed {:?}", relay_peer_id);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitEstablished {src_peer_id, limit})) => {
+                    println!("5 InboundCircuitEstablished {:?}", src_peer_id);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitReqFailed {relay_peer_id, error})) => {
+                    println!("6 InboundCircuitReqFailed {:?}", relay_peer_id);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitReqDenied {src_peer_id})) => {
+                    println!("7 InboundCircuitReqDenied {:?}", src_peer_id);
+                },
+                SwarmEvent::Behaviour(my_behavior::Event::RelayClient(v2::client::Event::InboundCircuitReqDenyFailed {src_peer_id, error})) => {
+                    println!("8 InboundCircuitReqDenyFailed {:?}", src_peer_id);
+                },
 
                 // DCUtR
                 SwarmEvent::Behaviour(my_behavior::Event::Dcutr(dcutr::behaviour::Event::InitiatedDirectConnectionUpgrade {remote_peer_id, local_relayed_addr})) => {
                     println!("1 InitiatedDirectConnectionUpgrade {:?}", remote_peer_id);
                 },
                 SwarmEvent::Behaviour(my_behavior::Event::Dcutr(dcutr::behaviour::Event::RemoteInitiatedDirectConnectionUpgrade {remote_peer_id, remote_relayed_addr})) => {
-                    println!("1 InitiatedDirectConnectionUpgrade {:?}", remote_peer_id);
+                    println!("2 RemoteInitiatedDirectConnectionUpgrade {:?}", remote_peer_id);
                 },
                 SwarmEvent::Behaviour(my_behavior::Event::Dcutr(dcutr::behaviour::Event::DirectConnectionUpgradeSucceeded {remote_peer_id})) => {
-                    println!("1 InitiatedDirectConnectionUpgrade {:?}", remote_peer_id);
+                    println!("3 DirectConnectionUpgradeSucceeded {:?}", remote_peer_id);
                 },
                 SwarmEvent::Behaviour(my_behavior::Event::Dcutr(dcutr::behaviour::Event::DirectConnectionUpgradeFailed {remote_peer_id, error})) => {
-                    println!("1 InitiatedDirectConnectionUpgrade {:?}", remote_peer_id);
+                    println!("4 DirectConnectionUpgradeFailed {:?}", remote_peer_id);
                 },
                 
 
